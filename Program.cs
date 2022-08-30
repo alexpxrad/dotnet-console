744365d650eca0611ef8b4f92b62667dd83cfd52
alexpxrad <alexpxrad@gmail.com>
2022-08-30 11:40:33 -0400

    bank of Boca Code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// test bank program
+Console.WriteLine("Welcome to Bank Boca Code");
+
+BankAccount account =  new BankAccount();  //The moment I put new, class becomes an instance
+account.AccountNumber = "12345";
+
+account.MakeDeposit(100);  
+
+BankAccount account2 = new SavingAccount(500);
+
+
+try {
+decimal newBalance = account2.MakeDepositGetBalance(-1);
+} catch(Exception ex) {
+    Console.WriteLine($"We got an error when running MakeDepositGetBalance(-1) ");
+}
+
+Console.WriteLine($"The balance of account is {account.Balance}");
+Console.WriteLine($"The Account Number of account is {account.AccountNumber}");
+
+
+Console.WriteLine($"The balance of account2 is {account2.Balance}");
+
